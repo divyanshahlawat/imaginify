fad2bcc21195dd1e9b80a3aabe259195f0a93d2c
Divyansh <divyanshahlawat035@gmail.com>
2024-09-04 23:41:57 +0530

    g

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,7 +10,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// ERROR HANDLER
 export const handleError = (error: unknown) => {
   if (error instanceof Error) {
     // This is a native JavaScript error (e.g., TypeError, RangeError)
